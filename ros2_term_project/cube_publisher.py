96defeb14af3866437c9217e8b08bb1ab1138200
준영 <128689935+ljy21123@users.noreply.github.com>
2023-11-22 16:55:30 +0900

    큐브 움직임 수정
    좌표를 활용하는 방법으로 수정

--- ros2_term_project/cube_publisher.py
+++ ros2_term_project/cube_publisher.py
@@ -1,8 +1,11 @@
+#!/usr/bin/env python3
 # ROS2용 패키지 임포트
 import rclpy
+from nav_msgs.msg import Odometry
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
 from threading import Thread, Event
+import math
 
 
 class CubePublisher(Node):  # Node 상속
@@ -13,29 +16,58 @@ class CubePublisher(Node):  # Node 상속
         # Node의 node_name 초기화
         super().__init__('cube_publisher')
         # Publisher 객체 생성
-        self.publisher_ = self.create_publisher(Twist, 'cube_cmd_vel', 1)
+        self.publisher_ = self.create_publisher(Twist, '/CUBE/cmd_demo', 10)
         timer_period = 1 / CubePublisher.PUB_RATE  # seconds
         # Timer 객체 생성
         self.timer = self.create_timer(timer_period, self.timer_callback)
         # 선속도 인스턴스 변수 정의
-        self.current_linear_x = -1.0
-        # 방향 전환을 위한 별도의 스레드
-        self.thread = Thread(target=self.turn_cube, daemon=True)
-        self.thread.start()
+        self.current_linear_y = 1.0
+
+        # Odometry 데이터를 받기 위한 subscriber 추가
+        self.subscription = self.create_subscription(Odometry, '/CUBE/odom', self.odom_callback, 10)
+        # 시작 위치 설정
+        self.start_position = (34.997495, -64.0)
+        # 목표 위치 설정
+        self.target_position = (34.997495, -77.0)
+        # 방향 전환 했는지 확인하는 변수
+        self.turned_state = False
 
     # Timer 콜백 메서드 정의
     def timer_callback(self):
         msg = Twist()   # Twist 객체 생성
-        msg.linear.x = float(self.current_linear_x) # 선속도 할당
-        self.get_logger().info('linear.x = %.3f' % msg.linear.x)    # info
+        msg.linear.y = float(self.current_linear_y) # 선속도 할당
         self.publisher_.publish(msg)    # 토픽 발행
 
-    # 방향 전환 메소드
+    # Odometry 콜백 메서드 정의
+    def odom_callback(self, msg):
+        # 현재 위치 추출
+        # pose.pose: 포즈 정보.실제 포즈 데이터
+        current_position = (msg.pose.pose.position.x, msg.pose.pose.position.y)
+
+        # 현재 위치와 목표 위치 간의 거리 계산
+        distance_target = math.sqrt((self.target_position[0] - current_position[0]) ** 2 +
+                                    (self.target_position[1] - current_position[1]) ** 2)
+        distance_start = math.sqrt((self.start_position[0] - current_position[0]) ** 2 +
+                                   (self.start_position[1] - current_position[1]) ** 2)
+
+        # 일정 거리 내에 있으면 turn_cube 메서드 호출
+        if distance_target < 0.01 and not self.turned_state:
+            self.turn_cube()
+            self.turned_state = True
+        elif distance_start < 0.01 and self.turned_state:
+            self.turn_cube()
+            self.turned_state = False
+
+    # 방향 전환 메서드
     def turn_cube(self):
-        while True:
-            Event().wait(14.0)  # 14초마다 실행
-            self.current_linear_x = -self.current_linear_x
-            self.get_logger().info('turn success!!@')
+        # while True:
+        self.current_linear_y = -self.current_linear_y
+        # self.get_logger().info('turn success!!')
+
+    def stop(self):
+        msg = Twist()  # Twist 객체 생성
+        msg.linear.y = 0.0
+        self.publisher_.publish(msg)  # 토픽 발행
 
 
 def main(args=None):
@@ -47,13 +79,13 @@ def main(args=None):
     try:
         rclpy.spin(cube_publisher)
     except KeyboardInterrupt:
-        # Ctrl-C 누를 때 메시지 출력
-        cube_publisher.get_logger().info('user-terminated by pressing Ctrl-C...')
+        cube_publisher.stop()
 
-    # rclpy.shutdown()에서 자동으로 처리하기 때문에 반드시 사용할 필요는 없음
-    cube_publisher.destroy_node()
     # ROS2 컨텍스트 종료
     rclpy.shutdown()
+
+
+main()
 
 
 if __name__ == '__main__':
